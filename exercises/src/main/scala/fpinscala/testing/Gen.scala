012da05eda39dea2d968cc4f9c4ce5109a4e3cce
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2019-12-30 21:42:27 -0800

    Fix applicative exercise not compiling

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -6,29 +6,27 @@ import fpinscala.parallelism._
 import fpinscala.parallelism.Par.Par
 import Gen._
 import Prop._
-import java.util.concurrent.{Executors,ExecutorService}
+import java.util.concurrent.{Executors, ExecutorService}
 
 /*
 The library developed in this chapter goes through several iterations. This file is just the shell, which you can fill in and modify while working through the chapter.
-*/
+ */
 
-trait Prop {
-}
+trait Prop {}
 
 object Prop {
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
 object Gen {
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = ???
+
   def unit[A](a: => A): Gen[A] = ???
 }
 
-trait Gen[A] {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+case class Gen[A](sample: State[RNG, A]) {
+  def map[A, B](f: A => B): Gen[B] = ???
+  def flatMap[A, B](f: A => Gen[B]): Gen[B] = ???
 }
 
-trait SGen[+A] {
-
-}
-
+trait SGen[+A] {}
